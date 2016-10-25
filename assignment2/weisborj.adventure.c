6b292dde662230ebc0b06fc840c4b3404fada161
julianweisbord <julianweisbord@gmail.com>
2016-10-25 11:52:03 -0700

    Creating rooms
    Signed-off-by: julianweisbord <julianweisbord@gmail.com>

--- assignment2/weisborj.adventure.c
+++ assignment2/weisborj.adventure.c
@@ -7,8 +7,8 @@
 #include <time.h>
 
 #define MAX_STR_LEN 20
-#define NUM_OF_ROOMS 10
-
+#define DIFF_ROOMS 10
+#define NUM_ROOMS 7
 //How we compare rooms
 enum Room_type{
   START_ROOM,
@@ -18,7 +18,8 @@ enum Room_type{
 
 struct Room{
   char* name;
-  int connections;
+  int connection_num; //number of connections
+  int* connectionArray; //Array of integer indexes to the Game roomArray
   enum Room_type roomType;
 };
 
@@ -31,33 +32,34 @@ struct Game{
 
 // Prototypes
 char* get_room_name(int roomNum);
-struct Room* init_stuff(struct Room dungeon);
-struct Game* init_game(struct Game adventure);
+void init_game(struct Game* adventure);
 int randNum(int start, int end);
+int check_win(struct Room* possibleEnd);
+void add_connection(struct Game *adventure, int index);
+
 
 int main(int argc, char** argv){
 
 
-  //Create directory
+  //Create directory with name: weisborj.rooms.<pid>
   int pid = getpid();
   char directory[MAX_STR_LEN];
   memset(directory, '\0', MAX_STR_LEN);
   char *user_rooms = "weisborj.rooms.";
   snprintf(directory, MAX_STR_LEN, "%s%d", user_rooms, pid);
   printf("%s\n", directory);
-  mkdir(directory, 0555);
+  mkdir(directory, 0666);
 
 
   struct Room room1, *roomPointer;
   struct Game adventure, *adventurePointer;
   roomPointer = &room1;
   adventurePointer = &adventure;
+  // initializing struct member variables
+  init_game(adventurePointer);
+  //init_room(roomPointer);
 
 
-  // init_room(Room room1)
-  // room1.name = malloc(sizeof(char) * (MAX_STR_LEN+1));
-  // strcpy(room1.name, get_room_name(num));
-  // printf("%s \n", room1.name);
   return 0;
 
 }
@@ -106,19 +108,30 @@ char* get_room_name(int roomNum){
 }
 
 //init_game Function//
-struct Game* init_game(struct Game adventure){
-  struct Game* gamePointer = malloc(sizeof(struct Game));
-  gamePointer->numSteps =0;
-  return gamePointer;
+void init_game(struct Game* adventure){
+  adventure = malloc(sizeof(struct Game));
+  adventure->numSteps =0;
+  adventure->numOfRooms = NUM_ROOMS;
+  adventure->roomArray = malloc(sizeof(struct Room *) * NUM_ROOMS);
+  printf("MIDDLE OF INIT GAME\n");
+
+  int i;
+  for (i = 0; i < NUM_ROOMS; ++i) {
+    printf("Ahuuh-haaaawuh\n");
+
+    adventure->roomArray[i] = malloc(sizeof(struct Room));
+    adventure->roomArray[i]->name = malloc(sizeof(char) * (MAX_STR_LEN+1));
+    adventure->roomArray[i]->connection_num = 0;
+
+    adventure->roomArray[i]->connectionArray = malloc(sizeof(int) *50);
+    printf("END OF INIT GAME!\n");
+
+    // add_connection(adventurePointer, i);
+  }
+
+
 }
 
-//int_room Function//
-struct Room* init_room(struct Room dungeon){
-  struct Room* roomPointer = malloc(sizeof(struct Room));
-  roomPointer->name = malloc(sizeof(char) * (MAX_STR_LEN+1));
-  return roomPointer;
-
-}
 //randNum Function//
 int randNum(int start, int end){
   srand(time(NULL));
@@ -126,6 +139,26 @@ int randNum(int start, int end){
   // This hack isn't as random but will work for the purpose of printing a number in a range.
   if(r+ start <= end)
     r+=start;
-  printf("Random Number: %d", r);
+  printf("Random Number: %d\n", r);
   return r;
+}
+//To check if the user is in the end room//
+int check_win(struct Room* possibleEnd){
+  if (possibleEnd->roomType == END_ROOM){
+    printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
+    return 1;
+  }
+  return 0;
+}
+//Initialize all connections//
+void add_connection(struct Game *adventure, int index){
+  //call this function for each room
+  int random = randNum(3,6);
+  struct Room* room = adventure->roomArray[index];//check this
+  while(room->connection_num < random){
+    do{
+      room->connectionArray[room->connection_num] = randNum(1,NUM_ROOMS);
+    }while(room->connectionArray[room->connection_num] == index);
+  }
+
 }
